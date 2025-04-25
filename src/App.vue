028da4337234a1f382c4e0c263fecb480f1b2d83
Morris235 <dltkdah777@gmail.com>
2025-04-25 14:22:56 +0900

    feat : ingress, k8s 적용

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="plus">
+    <h1>덧셈 기능 만들기</h1>
+    <label>num: 1</label><input type="text" v-model="num1">&nbsp;
+    <label>num: 2</label><input type="text" v-model="num2">&nbsp;
+    <button @click="sendPlus">더하기</button>
+    <hr>
+    <p>{{ num1 }} + {{ num2 }} = {{ sum }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const num1 = ref(0);
+const num2 = ref(0);
+const sum = ref(0);
+
+const sendPlus = async () => {
+  // 6. 백엔드, 프론트 X (kubernetes의 ingress 추가 후)
+  const response = await fetch('http://api.localtest.me/plus', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;cahrset=utf-8;'
+    },
+    body: JSON.stringify({ num1: num1.value, num2: num2.value })
+  });
+
+  const data = await response.json();
+  console.log('data:', data);
+  sum.value = data.sum;
+}
+
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.plus {
+  text-align: center;
 }
 </style>
